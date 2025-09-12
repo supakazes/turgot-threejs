e9305d65363a35448cc1bf22798544c9abd937c8
Florian BOUDOT <florian.boudot@adevinta.com>
2025-09-12 16:35:15 +0200

    refactor: remove unused files

--- src/constants/mapLngLatRef.ts
+++
@@ -1,6 +0,0 @@
-const mapLngLatRef = {
-  topLeft: { lng: 2.396332303381996, lat: 48.849789392538895 },
-  bottomRight: { lng: 2.2976630443753265, lat: 48.863609838069664 },
-};
-
-export default mapLngLatRef;
